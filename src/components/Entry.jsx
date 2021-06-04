af39480887a30ba71cc505d53c53dcd5aa375026
Bhavya Wahie <bhavyawahie@gmail.com>
2021-06-05 03:13:38 +0530

    errors were corrected and prop functionality was reimplemented

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,17 +1,14 @@
 import React from "react"
 
-const Entry = () => {
+const Entry = (props) => {
     return <div className="term">
             <dt>
                 <span className="emoji" role="img" aria-label="Tense Biceps">
-                    💪
+                    {props.emojiUnicode}
                 </span>
-                <span>Tense Biceps</span>
+                <span>{props.name}</span>
             </dt>
-            <dd>
-                “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-                used in connection with doing sports, e.g. at the gym.
-            </dd>
+            <dd>{props.meaning}</dd>
         </div>
         }
 export default Entry
